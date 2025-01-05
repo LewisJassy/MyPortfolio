bf25f0d21ae5f6ab83d5c54582abb55fc6ecdc62
LewisJassy <jassylewis70@gmail.com>
2025-01-05 17:22:19 +0300

    initialize portfolio project with Next.js, Tailwind CSS, and ESLint configuration

---
+++ portfolio/app/layout.js
@@ -0,0 +1,28 @@
+import { Outfit as OutfitFont, Ovo as OvoFont } from "next/font/google";
+import "./globals.css";
+
+const Outfit = OutfitFont({
+  subsets: ["latin"], weight: ["400", "500", "600", "700"]
+});
+
+const Ovo = OvoFont({
+  subsets: ["latin"], weight: ["400"]
+});
+
+
+export const metadata = {
+  title: "Portfolio",
+  description: "My portfolio",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${Outfit.className} ${Ovo.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
