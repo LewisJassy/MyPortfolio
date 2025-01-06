e340636ec43a93a03d289e5bbe0a3d00b2c8efab
LewisJassy <jassylewis70@gmail.com>
2025-01-06 12:34:03 +0300

    refactor: update Navbar component for improved styling and asset imports

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { assets } from '../../assets/assets'
+import { assets } from '@/assets/assets'
 import Image from 'next/image'
 
 const navbar = () => {
@@ -9,16 +9,16 @@ const navbar = () => {
         <a href="">
             <Image src={assets.logo1} alt="logo" className='w-28 cursor-pointer mr-14' />
         </a>
-        <ul>
-            <li><a href="#top">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#work">My Work</a></li>
-            <li><a href="#contact">Contact Me</a></li>
+        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <li><a className="font-Ovo" href="#top">Home</a></li>
+            <li><a className="font-Ovo" href="#about">About</a></li>
+            <li><a className="font-Ovo" href="#services">Services</a></li>
+            <li><a className="font-Ovo" href="#work">My Work</a></li>
+            <li><a className="font-Ovo" href="#contact">Contact Me</a></li>
         </ul>
 
         <div>
-            <a href="#contact">Contact <Image src={assets.arrow_icon} alt='arrow_icon' className='w-3'/></a>
+            <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo">Contact <Image src={assets.arrow_icon} alt='arrow_icon' className='w-3'/></a>
         </div>
       </nav>
     </>
