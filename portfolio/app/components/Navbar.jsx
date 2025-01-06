0916d115d5fa8795f537272209c597237d286c93
LewisJassy <jassylewis70@gmail.com>
2025-01-06 11:31:16 +0300

    refactor: update navbar component and improve CSS imports

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import { assets } from '../../assets/assets'
+import Image from 'next/image'
 
 const navbar = () => {
   return (
     <>
-      <nav>
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between bg-white z-50'>
         <a href="">
-            <image src={assets.logo} className='w-28 cursor-pointer mr-14' />
+            <Image src={assets.logo} alt="logo" className='w-28 cursor-pointer mr-14' />
         </a>
         <ul>
             <li><a href="#top">Home</a></li>
@@ -16,7 +18,7 @@ const navbar = () => {
         </ul>
 
         <div>
-            <a href="#contact">Contact <image src={assets.arrow_icon} className='w-3'/></a>
+            <a href="#contact">Contact <Image src={assets.arrow_icon} alt='arrow_icon' className='w-3'/></a>
         </div>
       </nav>
     </>
--- portfolio/app/page.js
+++ portfolio/app/page.js
@@ -1,11 +1,10 @@
-"use client"
-import navbar from "./components/navbar";
-import Image from "next/image";
+'use client'
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
-    <navbar />
+    <Navbar />
     </>
   );
 }
