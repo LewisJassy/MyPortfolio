4be52b55e4cabdf524d3472a1616d4e10bc46f31
LewisJassy <jassylewis70@gmail.com>
2025-01-07 11:41:40 +0300

    fix: correct typos in class names and improve code formatting in Navbar component

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -15,10 +15,10 @@ const navbar = () => {
 
   return (
     <>
-    <div className='fixed tp-0 right-0 w-11/12 -z-10 transalte-y-[-80%]'>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
       <Image src={assets.header_bg_color} alt="" className="w-full" priority />
     </div>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between bg-white z-50'>
+      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
         <a href="">
             <Image src={assets.logo1} alt="logo" className='w-28 cursor-pointer mr-14' />
         </a>
@@ -35,7 +35,8 @@ const navbar = () => {
           <button>
             <Image src={assets.moon_icon} alt='' className="w-6" />
           </button>
-            <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo">Contact <Image src={assets.arrow_icon} alt='arrow_icon' className='w-3'/></a>
+            <a href="#contact" className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo">
+              Contact <Image src={assets.arrow_icon} alt='arrow_icon' className='w-3'/></a>
             <button className="block md:hidden ml-3" onClick={openMenu}>
               <Image src={assets.menu_black} alt="" className='w-6' />
             </button>
