92d27a0498d9ea13bbbbdcb93bf80065c8f8df61
LewisJassy <jassylewis70@gmail.com>
2025-01-15 23:13:42 +0300

    feat: add scroll effect to Navbar component for dynamic background and styling

--- portfolio/app/components/Navbar.jsx
+++ portfolio/app/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 import { assets } from '@/assets/assets'
 import Image from 'next/image'
 
 const navbar = () => {
   const sideMenuRef = useRef()
+
+  const [isScroll, setIsScroll] = useState(false)
 
   const openMenu = () => {
     sideMenuRef.current.style.transform = 'translateX(-16rem)'
@@ -13,16 +15,27 @@ const navbar = () => {
     sideMenuRef.current.style.transform = 'translateX(16rem)'
   }
 
+  useEffect(()=>{
+    window.addEventListener('scroll', ()=>{
+        if(scrollY > 50){
+          setIsScroll(true)
+        }
+        else{
+          setIsScroll(false)
+        }
+    })
+  })
+
   return (
     <>
     <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
       <Image src={assets.header_bg_color} alt="" className="w-full" priority />
     </div>
-      <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
-        <a href="">
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm" : ""}`}>
+        <a href="#top">
             <Image src={assets.logo1} alt="logo" className='w-28 cursor-pointer mr-14' />
         </a>
-        <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
+        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50"} `}>
             <li><a className="font-Ovo" href="#top">Home</a></li>
             <li><a className="font-Ovo" href="#about">About</a></li>
             <li><a className="font-Ovo" href="#services">Services</a></li>
